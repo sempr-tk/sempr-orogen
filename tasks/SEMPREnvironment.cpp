2c8eeaa44c822c13e74c8c2be46a53274ef2261f
Nils <nils.niemann@dfki.de>
2018-04-18 09:12:49 +0200

    initial commit by rock-create-orogen

---
+++ tasks/SEMPREnvironment.cpp
@@ -0,0 +1,62 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "SEMPREnvironment.hpp"
+
+using namespace sempr;
+
+SEMPREnvironment::SEMPREnvironment(std::string const& name)
+    : SEMPREnvironmentBase(name)
+{
+}
+
+SEMPREnvironment::SEMPREnvironment(std::string const& name, RTT::ExecutionEngine* engine)
+    : SEMPREnvironmentBase(name, engine)
+{
+}
+
+SEMPREnvironment::~SEMPREnvironment()
+{
+}
+
+bool SEMPREnvironment::addObjectAssertion(::sempr_rock::ObjectAssertion const & arg0)
+{
+    return bool();
+}
+
+::sempr_rock::SPARQLResult SEMPREnvironment::answerQuery(::std::string const & arg0)
+{
+    return ::sempr_rock::SPARQLResult();
+}
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See SEMPREnvironment.hpp for more detailed
+// documentation about them.
+
+bool SEMPREnvironment::configureHook()
+{
+    if (! SEMPREnvironmentBase::configureHook())
+        return false;
+    return true;
+}
+bool SEMPREnvironment::startHook()
+{
+    if (! SEMPREnvironmentBase::startHook())
+        return false;
+    return true;
+}
+void SEMPREnvironment::updateHook()
+{
+    SEMPREnvironmentBase::updateHook();
+}
+void SEMPREnvironment::errorHook()
+{
+    SEMPREnvironmentBase::errorHook();
+}
+void SEMPREnvironment::stopHook()
+{
+    SEMPREnvironmentBase::stopHook();
+}
+void SEMPREnvironment::cleanupHook()
+{
+    SEMPREnvironmentBase::cleanupHook();
+}
