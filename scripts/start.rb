a6a615fc130a9a83586ea30640fea906147775f3
Nils <nils.niemann@dfki.de>
2018-09-12 14:02:16 +0200

    default params in start.rb

--- scripts/start.rb
+++ scripts/start.rb
@@ -7,6 +7,8 @@ Orocos.initialize
 Orocos.run 'sempr::SEMPREnvironment' => 'sempr' do
     sempr = Orocos.name_service.get 'sempr'
 
+    sempr.rdf_file = "../resources/model.owl"
+    sempr.rules_file = "../resources/owl.rules"
     sempr.configure
     sempr.start
 
